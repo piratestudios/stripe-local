a93fd1a23e650b2bc0725b22354b0a1385110f13
Alexander Martin <alex@alxmrtn.com>
2019-07-09 13:29:31 -0500

    Add request signing functionality (#4)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import assert from 'assert'
 import Stripe from 'stripe'
 import axios from 'axios'
+import crypto from 'crypto'
 
 module.exports = (opts = {}) => {
   assert(typeof opts === 'object', 'Options must be an object')
   assert(opts.secretKey, 'Must pass your Stripe secret key!')
   assert(opts.webhookUrl, 'Must pass a webhook URL!')
 
+  opts.webhookSecret = opts.webhookSecret || false
   opts.quiet = opts.quiet || false
   opts.interval = opts.interval
     ? parseInt(opts.interval, 10)
@@ -14,6 +16,9 @@ module.exports = (opts = {}) => {
 
   const stripe = Stripe(opts.secretKey)
   const currentTimeStamp = () => Math.floor(Date.now() / 1000)
+  const generateSignature = (secret, payload) => crypto.createHmac('sha256', secret)
+      .update(payload, 'utf8')
+      .digest('hex')
 
   const request = axios.create({
     timeout: 5000,
@@ -37,8 +42,13 @@ module.exports = (opts = {}) => {
         return stripe.events.retrieve(evt.id)
           .then(data => {
             !opts.quiet && console.log(`Received Stripe Event: ${evt.id}`)
-
-            return request.post(opts.webhookUrl, data)
+            let headers = {}
+            if (opts.webhookSecret) {
+              console.log(`Signing Event: ${evt.id}`)
+              let signature = generateSignature(opts.webhookSecret, `${lastTimestamp}.${JSON.stringify(data)}`)
+              headers['stripe-signature'] = `t=${lastTimestamp},v1=${signature}`
+            }
+            return request.post(opts.webhookUrl, data, { headers })
               .then(res => !opts.quiet && console.log(res.data))
           })
       })).catch(err => console.error(err))
